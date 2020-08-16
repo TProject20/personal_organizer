ec008bd35092d61e986d2c1067ea19d5f268eded
Arev Suleymanyan <kivera16@gmail.com>
2020-08-16 12:17:50 +0300

    մի թեթև փոփոխություն

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Signin() {
-    return (
-        <div>
-            <h1>Signin</h1>
-        </div>
-    )
+	return (
+		<div>
+			<h1 style={{ color: "white" }}>Signin</h1>
+			<input type="text" placeholder="login" />
+			<br />
+			<input type="password" placeholder="password" />
+			<br />
+			<input type="submit" />
+		</div>
+	);
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Signup() {
     return (
         <div>
-            <h1>Signup</h1>
+            <h1 style={{color :'white'}}>Signup</h1>
+            <input type='text' placeholder='name'/>
+            <br/>
+            <input type='text' placeholder='surname'/>
+            <br/>
+            <input type='password' placeholder='password'/>
+            <br/>
+            <input type='submit'/>
         </div>
     )
 }
