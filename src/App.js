6822a5ffa9467b14dff1dc6096e132c495d7bd55
Mary Tadevosyan <mary.tadevosyan92@gmail.com>
2020-08-17 20:31:10 +0400

    new register

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from "react";
+
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GetStarted from "./components/GetStarted";
-import About from "./components/About";
-import Signup from "./components/Signup";
-import Signin from "./components/Signin";
-import "./App.css";
+// import About from "./components/About";
+// import Signup from "./components/Signup";
+// import Signin from "./components/Signin";
 
-function App() {
+import HomePage from './components/HomePage/index';
+import "./App.css";
+import fire from "./config/fire";
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			user: null
+		}
+	}
+
+	componentDidMount(){
+		this.authListener()
+	}
+	authListener(){
+		fire.auth().onAuthStateChanged((user) => {
+			if(user){
+				this.setState({user});
+			}else{
+				this.setState({user:null});
+			}
+		});
+	}
+		
+	
+   render() {
 	return (
 		<Router>
 			<div className="App">
+				
 				<ul>
 					<li> <Link to="/">Home</Link> </li>
 					<li>
@@ -18,20 +44,20 @@ function App() {
 					<li>
 						<Link to="/signup">Sign up</Link>
 					</li>
-					<li>
+					{/* <li>
 						<Link to="/about">About</Link>
-					</li>
+					</li> */}
 				</ul>
 				
 				<Switch>
 					<Route path='/' exact component={GetStarted}/>
-					<Route path="/signin" component={Signin} />
-					<Route path="/signup" component={Signup} />
-					<Route path="/about" component={About} />
+					<Route path="/signin" component={HomePage}/>
+					{/* <Route path="/about" component={Login} /> */}
 				</Switch>
 			</div>
 		 </Router>
 	);
+}
 }
 
 export default App;
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,14 +1,14 @@
-import React from "react";
+// import React from "react";
 
-export default function Signin() {
-	return (
-		<div className='signin'>
-			<h1 style={{ color: "white" }}>Signin</h1>
-			<input type="text" placeholder="login" />
-			<br />
-			<input type="password" placeholder="password" />
-			<br />
-			<input type="submit" />
-		</div>
-	);
-}
+// export default function Signin() {
+// 	return (
+// 		<div className='signin'>
+// 			<h1 style={{ color: "white" }}>Signin</h1>
+// 			<input type="text" placeholder="login" />
+// 			<br />
+// 			<input type="password" placeholder="password" />
+// 			<br />
+// 			<input type="submit" />
+// 		</div>
+// 	);
+// }
