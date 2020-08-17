f373d21d601ef49f0a21af46a7a9849c88531075
Arev Suleymanyan <kivera16@gmail.com>
2020-08-17 09:30:04 +0300

    փոփոխություն

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GetStarted from "./components/GetStarted";
-import About from './components/About'
-import Signup from './components/Signup'
-import Signin from './components/Signin'
+import About from "./components/About";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
 import "./App.css";
-// import Calendar from "./calendar/Calendar";
-
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
-				{/* <Calendar/> */}
-				
 				<ul>
 					<li>
 						<Link to="/signin">Sign in</Link>
@@ -27,19 +23,12 @@ function App() {
 				</ul>
 				<GetStarted />
 				<Switch>
-        
-					<Route path="/signin">
-						<Signin />
-					</Route>
-					<Route path="/signup">
-							<Signup />
-					</Route>
-					<Route path="/about">
-							<About />
-					</Route>
+					<Route path="/signin" component={Signin} />
+					<Route path="/signup" component={Signup} />
+					<Route path="/about" component={About} />
 				</Switch>
 			</div>
-		 </Router>
+		</Router>
 	);
 }
 
