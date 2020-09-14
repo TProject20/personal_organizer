d7fefda21f47f5725a320c7de98494d68558c82b
Arev Suleymanyan <kivera16@gmail.com>
2020-09-14 16:47:24 +0400

    delete transition group

--- src/TodoList/Notes.js
+++ src/TodoList/Notes.js
@@ -1,29 +1,25 @@
 import React from "react";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "./style.scss";
 
 export const Notes = ({ notes, onRemove }) => (
-	<TransitionGroup component="ul" className="list-group">
+
+	<ul className="list group">
 		{notes.map((note) => (
-			<CSSTransition
-			key={note.id}
-			classNames={'note'}
-			timeout={800}
-			>
-				<li className="list-group-item note" >
-					<div>
-						<span>{note.title}</span>
-						<small>{note.date}</small>
-					</div>
-					<button
-						type="button"
-						onClick={() => onRemove(note.id)}
-						className="btn btn-outline-danger btn-sm"
-					>
-						&times;
-					</button>
-				</li>
-			</CSSTransition>
+			<li className="list-group-item note" key={note.id}>
+				<div>
+					<span>{note.title}</span>
+					<small>{note.date}</small>
+				</div>
+				<button
+					type="button"
+					onClick={() => onRemove(note.id)}
+					className="btn btn-outline-danger btn-sm"
+				>
+					&times;
+				</button>
+			</li>
 		))}
-	</TransitionGroup>
+
+	</ul>
+
 );
